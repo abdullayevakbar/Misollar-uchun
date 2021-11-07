61420a6b1a00f7555aeed4a911a588633620665c
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2021-11-07 18:29:56 +0500

    none

---
+++ misollar_uchun/type_conversion/python03.py
@@ -0,0 +1,9 @@
+#Create a variable called 'var_int' and assign with data type integer to it.
+
+#Create a variable called 'var_float' and assign with data type float to it.
+
+#Create a variable called 'answer' and assign it the multiply of var_int and var_float.
+
+#Print the value of answer
+
+#Print the type of answer.
---
+++ misollar_uchun/type_conversion/python04.py
@@ -0,0 +1,11 @@
+#Create a variable called 'var_int' and assign with data type integer to it.
+
+#Convert 'var_int' to str data type and assign to another variable called 'var_str'
+
+#Create a variable called 'number' and find the remainder of the division of 'var_int' by 5.
+
+#Create a variable called 'answer' and assign it the multiply of number and var_str.
+
+#Print the value of answer
+
+#Print the type of answer.

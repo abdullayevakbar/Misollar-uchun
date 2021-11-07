06f6614395949c76f9f86492417b6f82335cbf8f
Akbar Abdullayev <91420868+abdullayevakbar@users.noreply.github.com>
2021-11-07 18:17:40 +0500

    Delete misollar uchun directory

--- misollar uchun/Basic_Variables/python02.py
+++
@@ -1,7 +0,0 @@
-#Create a variable S and assign it the value of the string "Hello teacher"
-
-#Print the value of S
-
-#Create a variable C and assign it the value of the string "My teacher Zarif"
-
-#print the value of C
